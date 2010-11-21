70df21f55b04d7fffdc1807ece56cb1805132c24
pwolanin <pwolanin@voter-solr.(none)>
2010-11-21 16:01:04 +0000

    cleanup JAVA_OPTS

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+# Usage: ./start.sh [port]
+
 
 # Make sure we are in the right directory
 cd `dirname $0`
 
+# Default port is 8983
 port=8983
 
 if [ -n "$1" ]; then
@@ -18,13 +21,13 @@ fi
 
 echo "Starting Solr on port $port"
 
+JAVA_OPTS="$JAVA_OPTS -DSTOP.PORT=8079 -DSTOP.KEY=secret -Xmx128m"
+JAVA_OPTS="$JAVA_OPTS -Dsolr.solr.home=./solr -Djetty.home=."
+JAVA_OPTS="$JAVA_OPTS -Djetty.port=$port -Djetty.logs=./logs"
+export JAVA_OPTS
+
 rm -f logs/console.log
-nohup java -DSTOP.PORT=8079 -DSTOP.KEY=secret -Xmx128m \
-           -Dsolr.solr.home=./solr \
-           -Djetty.home=. \
-           -Djetty.port=$port \
-           -Djetty.logs=./logs \
-           -jar start.jar 2> ./logs/console.log  &
+nohup java $JAVA_OPTS -jar start.jar 2> ./logs/console.log  &
 
 # Write the process ID into the file "pid"
 echo $! > ./logs/solr.pid
